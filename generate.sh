5eaeba4e0be9e26844a11de7839846f91d11b045
Andrew Klapper <ak475826@gmail.com>
2026-07-22 07:54:34 -0400

    feat: implement generic ReAct engine, protobuf validation, and client-side JetStream routing

--- generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Compiling Go stubs..."
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    *.proto
-
-echo "Compiling C# stubs..."
-cd ../artemis_link
-dotnet build
